65fb77eed92c1927b567a2820917ba837934ffe1
Phanupong-M <panupong38240@gmail.com>
2025-03-28 12:23:04 +0700

    feat: setup tailwind css

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function LandingPage() {
+  
+  return (
+    <div className="text-3xl font-bold underline">init project</div>
+  );
+}
+
+export default LandingPage;
+
